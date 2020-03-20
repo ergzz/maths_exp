4308ee4fb58948670e5b48c6f657ec43db352db4
ergzz <eva.ragazzini@hotmail.fr>
2020-03-20 20:48:39 +0100

    added append to list + count

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(2000)
+
 '''Choisir la taille du plateau'''
 N = int(input("Taille du plateau:"))
 '''Choisir la case de départ'''
@@ -11,6 +14,7 @@ mvmt_x = [1, 1, -1, -1, 2, 2, -2, -2]
 mvmt_y = [2, -2, 2, -2, 1, -1, 1, -1]
 
 visit = [] #liste vide pour ajouter cases déja visitées
+steps = 0
 
 def valide(i, j): #regarde si case dans le plateau et pas déja visitée
     if ((i>=1) and (i<=N) and (j>=1) and j<=N): 
@@ -18,23 +22,23 @@ def valide(i, j): #regarde si case dans le plateau et pas déja visitée
             return True
     return False
 
-def possiblePath(i,j):
+def possiblePath(i,j,steps):
     #test qui return true si arrivée atteint
     if i == x_ar and j == y_ar:
-        #print("ex") #test
+        print(steps)
+        print(visit)
         return True
 
-    for k in range(len(mvmt_x)+1): #longueur de la liste des différents mouvements
+    for k in range(0,8): #longueur de la liste des différents mouvements
         next_x = i + mvmt_x[k]
         next_y = j + mvmt_y[k] #test mouvement pour chaque 
         if valide(next_x, next_y):
-            if possiblePath(next_x,next_y): #recursion
-                #print(next_x, next_y)
+            visit.append((mvmt_x[k],mvmt_y[k]))
+            steps += 1
+            if (possiblePath(next_x,next_y,steps)): #recursion
                 return True 
         return False
 
-#def startTour(): visualisation du tour à faire 
 
-print(possiblePath(x,y))
-
+print(possiblePath(x,y,0))
             
